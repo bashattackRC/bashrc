7e52193cd0c51355c1ca72f7af228bfeec329f22
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-10-29 19:36:32 +0000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+export OMBVER="0.9"
+set +x # exit on errors
+
+# Clear the console to clean everything
+clear
+
+# Echo the project name
+echo -n '''
+ ██████╗ ██╗  ██╗    ███╗   ███╗██╗   ██╗    ██████╗  █████╗ ███████╗██╗  ██╗
+██╔═══██╗██║  ██║    ████╗ ████║╚██╗ ██╔╝    ██╔══██╗██╔══██╗██╔════╝██║  ██║
+██║   ██║███████║    ██╔████╔██║ ╚████╔╝     ██████╔╝███████║███████╗███████║
+██║   ██║██╔══██║    ██║╚██╔╝██║  ╚██╔╝      ██╔══██╗██╔══██║╚════██║██╔══██║
+╚██████╔╝██║  ██║    ██║ ╚═╝ ██║   ██║       ██████╔╝██║  ██║███████║██║  ██║
+ ╚═════╝ ╚═╝  ╚═╝    ╚═╝     ╚═╝   ╚═╝       ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+    Version '''; echo "$OMBVER"
+if [ ! -d ~/".omb" ]; then
+  echo "Oh My Bash is not installed."
+  exit 1
+fi
+
+# Update OMB
+echo "Updating..."
+git clone https://github.com/TylerMS887/ohmybash ~/.omb-git -q
+cat ~/.omb-git/omb_init.sh >> ~/.bashrc
+rm -rf ~/.omb
+mkdir ~/.omb
+cp -r ~/.omb-git/themes ~/.omb/themes
+cp -r ~/.omb-git/plugins ~/.omb/plugins
+rm -rf ~/.omb-git
+echo "Updated! Type the following command to use Oh My Bash:"
+echo "   exec /bin/bash"
