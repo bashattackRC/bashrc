9245c112440ab024bddcc35f718212f5514db5fa
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-10-29 23:06:51 +0000

    Update omb_init.sh

--- omb_init.sh
+++ omb_init.sh
@@ -10,3 +10,51 @@ done
 
 # Load Theme
 source ~/.omb/themes/omb-$theme
+
+# Add "omb" Pseudo-Program
+omb() {
+  if [[ "$1" == "" ]]; then
+    echo "Usage: omb [plugin|theme|update] [enable|list] [name]"
+    echo ""
+    echo "Commands:"
+    echo "  plugin  Manage plugins"
+    echo "  theme   Manage themes"
+    echo "  update  Update omb"
+    echo ""
+    echo "Options:"
+    echo "  plugin:"
+    echo "    enable   Enable a plugin"
+    echo "    disable  Disable a plugin"
+    echo "    list     List all plugins"
+    echo ""
+    echo "  theme:"
+    echo "    enable   Enable a theme"
+    echo "    list     List all themes"
+    echo ""
+    return
+  fi
+
+  if [[ "$1" == "plugin" ]]; then
+    if [[ "$2" == "enable" ]]; then
+      echo "Enabling plugin $3"
+    elif [[ "$2" == "disable" ]]; then
+      echo "Disabling plugin $3"
+    elif [[ "$2" == "list" ]]; then
+      echo "Listing plugins"
+    else
+      echo "Invalid option for plugin command"
+    fi
+  elif [[ "$1" == "theme" ]]; then
+    if [[ "$2" == "enable" ]]; then
+      echo "Enabling theme $3"
+    elif [[ "$2" == "list" ]]; then
+      echo "Listing themes"
+    else
+      echo "Invalid option for theme command"
+    fi
+  elif [[ "$1" == "update" ]]; then
+      exec bash -c "$(curl -fsSL https://raw.githubusercontent.com/TylerMS887/ohmybash/main/update.sh)"
+  else
+      echo "Invalid command"
+  fi
+}
