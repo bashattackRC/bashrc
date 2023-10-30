828af005000b43e3bf4cf7ba754db30f2822a7a5
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-10-30 00:10:09 +0000

    Update omb_init.sh

--- omb_init.sh
+++ omb_init.sh
@@ -1,3 +1,8 @@
+# Define dummy git trap in case git plugin is disabled.
+# git plugin can redefine the function
+
+_git-plugin-trap (){}
+
 # Define Functions for Extensions
 function refresh_theme() {
    source ~/.omb/themes/omb-$theme
