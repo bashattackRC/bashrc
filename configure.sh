28a936c883459358516c596a74d3bcf3d2fad706
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-19 16:28:46 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,7 +9,6 @@ while read -r file; do
   sed -i "s/@@VERSION@@/$omb_version/g" "$file"
   sed -i "s/@@PROJECTNAME@@/Oh My Bash/g" "$file"
   sed -i "s/@@VER@@/Oh My Bash/g" "$file"
-  sed -i "s/@@BASHPATH@@/$BASH_SEDIFIED/g" "$file"
   ((placeholders_done++))
   printf "\rAdding info to scripts... $placeholders_done files modded."
 done < ./manifest.txt
