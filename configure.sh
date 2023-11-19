8682f25ffcdf8abd9e8bb7205c98c79eb55ccddb
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-19 14:53:08 +0000

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,13 @@
+omb_version=0.5
+BASH_SEDIFIED=${BASH//\//\\/}
+printf "\rAdding info to scripts... "
+while read -r file; do
+  sed -i "s/@@VERSION@@/$omb_version/g" "$file"
+  sed -i "s/@@PROJECTNAME@@/Oh My Bash/g" "$file"
+  sed -i "s/@@VER@@/Oh My Bash/g" "$file"
+  sed -i "s/@@BASHPATH@@/$BASH_SEDIFIED/g" "$file"
+  ((placeholders_done++))
+  printf "\rAdding info to scripts... ($placeholders_done files modded)"
+done < ./manifest.txt
+echo
+unset placeholders_done
