cdbd6f31d4a2c3800071da49ce32bee240f232d8
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-19 16:32:59 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,6 +4,14 @@
 # This script
 BASH_SEDIFIED=${OMB_BASH_PATH//\//\\/}
 printf "\rAdding info to scripts... "
+# Find all files in the folders "themes" and "plugins" whose name starts with "omb-"
+find themes plugins -type f -name "omb-*" > ./manifest.txt
+# Find all files in the folder "help" whose file name ends with ".omb-help"
+find help -type f -name "*.omb-help" >> ./manifest.txt
+# Add the file in the working directory called omb_init.sh
+echo "omb_init.sh" >> ./manifest.txt
+
+# Loop through the files in manifest.txt and replace placeholders
 while read -r file; do
   sed -i "s/@@VERSION@@/$OMBVER/g" "$file"
   sed -i "s/@@PROJECTNAME@@/Oh My Bash/g" "$file"
@@ -13,3 +21,4 @@ while read -r file; do
 done < ./manifest.txt
 echo
 unset placeholders_done
+rm ./manifest.txt
